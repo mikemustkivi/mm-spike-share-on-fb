629d164dff6577620da61bfa0289e5e99bf174a8
Mike Mustkivi <mike.mustkivi@elisa.ee>
2018-08-07 20:32:29 +0300

    Changed a method to share: share_open_graph -> feed

--- src/scripts/sharinghelper.js
+++ src/scripts/sharinghelper.js
@@ -13,6 +13,35 @@ var SharingHelper = {
     var sharedData;
     console.log('share on facebook');
     sharedData = {
+      "method": "feed",
+      "link": Config.hostUrl + "/html/koerte_saar.html",
+      "name": "Koerte saar",
+      "caption": "Testimise video pealkiri",
+      "source": 'https://www.youtube.com/watch?v=dt__kig8PVU',
+      "description": "Koerte saar on lugu 12-aastasest Atari Kobayashist",
+      "picture": Config.hostUrl + "/images/9fd2e62c-981c-4ec9-a2ed-7a52a39ada8a-poster.png"
+    };
+
+    setTimeout(function () {
+      console.log('shareOnFacebook:', JSON.stringify(sharedData));
+    }, 1000);
+
+    FB.ui(sharedData,
+      // callback
+      function(response) {
+        console.log('posting response:', JSON.stringify(response));
+        if (response && !response.error_message) {
+          alert('Posting completed.');
+        } else {
+          alert('Error while posting.');
+        }
+      });
+  },
+  // nope
+  shareOnFacebookLikes: function (data) {
+    var sharedData;
+    console.log('share on facebook');
+    sharedData = {
       "method": "share_open_graph",
       "action_type": 'og.likes',
       "action_properties": JSON.stringify({
@@ -27,34 +56,6 @@ var SharingHelper = {
     }, 1000);
 
     FB.ui(sharedData,
-      function(response) {
-        console.log('posting response:', JSON.stringify(response));
-        if (response && !response.error_message) {
-          alert('Posting completed.');
-        } else {
-          alert('Error while posting.');
-        }
-    });
-  },
-  // nope
-  shareOnFacebookFeed: function (data) {
-    var sharedData;
-    console.log('share on facebook');
-    sharedData = {
-      "method": "feed",
-      "link": Config.hostUrl + "/html/koerte_saar.html",
-      "name": "Koerte saar",
-      "caption": "Testimise video pealkiri",
-      "description": "“Koerte saar” on lugu 12-aastasest Atari Kobayashist, kelle eestkostjaks on korrumpeerunud meer Kobayashi. Kui linnapea dekreediga küüditatakse kõik Megasaki City koerad tohutule prügimäele, kihutab Atari oma Junior-Turbo propellerlennukiga Prügisaarele, et päästa oma neljajalgne ihukaitsja Täpi. Koos uute krantsidest sõpradega asub Atari eepilisele teekonnale, millega määratakse terve Prefektuuri tulevik.",
-      "picture": Config.hostUrl + "/images/9fd2e62c-981c-4ec9-a2ed-7a52a39ada8a-poster.png"
-    };
-
-    setTimeout(function () {
-      console.log('shareOnFacebook:', JSON.stringify(sharedData));
-    }, 1000);
-
-    FB.ui(sharedData,
-      // callback
       function(response) {
         console.log('posting response:', JSON.stringify(response));
         if (response && !response.error_message) {
