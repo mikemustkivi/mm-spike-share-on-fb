e7c9fd3f4c719f5615f61f6b4a934e10ecafa5e1
Mike Mustkivi <mike.mustkivi@elisa.ee>
2018-08-08 15:23:40 +0300

    Share with share_open_graph

--- src/scripts/sharinghelper.js
+++ src/scripts/sharinghelper.js
@@ -13,6 +13,36 @@ var SharingHelper = {
     var sharedData;
     console.log('share on facebook');
     sharedData = {
+      method: 'share_open_graph',
+      action_type: 'og.shares',
+      action_properties: JSON.stringify({
+        object: {
+          'og:url': '"https://www.mikemustkivi.eu/dev/movies/1234.html"', // your url to share
+          'og:title': 'Koerte saar (jagamisviis share_open_graph)',
+          'og:description': '"“Koerte saar” on lugu 12-aastasest Atari Kobayashist, kelle eestkostjaks on korrumpeerunud meer Kobayashi. Kui linnapea dekreediga küüditatakse kõik Megasaki City koerad tohutule prügimäele, kihutab Atari oma Junior-Turbo propellerlennukiga Prügisaarele, et päästa oma neljajalgne ihukaitsja Täpi. Koos uute krantsidest sõpradega asub Atari eepilisele teekonnale, millega määratakse terve Prefektuuri tulevik."',
+          'og:image': 'https://www.mikemustkivi.eu/dev/images/9fd2e62c-981c-4ec9-a2ed-7a52a39ada8a-poster.png'
+        }
+      })
+    };
+    setTimeout(function () {
+      console.log('shareOnFacebook:', JSON.stringify(sharedData));
+    }, 1000);
+
+    FB.ui(sharedData,
+      // callback
+      function(response) {
+        console.log('posting response:', JSON.stringify(response));
+        if (response && !response.error_message) {
+          alert('Posting completed.');
+        } else {
+          alert('Error while posting.');
+        }
+      });
+  },
+  shareOnFacebookBasedOnHeadMetaOgTags: function (data) {
+    var sharedData;
+    console.log('share on facebook');
+    sharedData = {
       "method": "feed",
       "link": Config.hostUrl + "/movies/1234.html"
     };
